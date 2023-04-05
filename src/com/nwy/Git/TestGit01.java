74fff3fe13a813427adf9a16966b3c71e9ba1d0c
nwyGit <735979113@qq.com>
2023-04-05 10:37:11 +0800

    增加了主方法

--- src/com/nwy/Git/TestGit01.java
+++ src/com/nwy/Git/TestGit01.java
@@ -5,4 +5,9 @@ package com.nwy.Git;
  * @Date: 2023-04-05 - 10:11
  */
 public class TestGit01 {
+
+    public static void main(String[] args) {
+        System.out.println("============");
+    }
+
 }
