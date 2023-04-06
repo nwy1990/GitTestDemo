f766c43193d2666afc3c44de569608053758307c
nwyGit <735979113@qq.com>
2023-04-06 09:30:12 +0800

    增加了主方法

---
+++ src/com/nwy/Git02/StartUp.java
@@ -0,0 +1,12 @@
+package com.nwy.Git02;
+
+/**
+ * @Auther: Nwy
+ * @Date: 2023-04-06 - 9:29
+ */
+public class StartUp {
+
+    public static void main(String[] args) {
+        System.out.println("我是天才");
+    }
+}
